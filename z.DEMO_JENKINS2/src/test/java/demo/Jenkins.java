5b099dfd4d31df9d6d4d563e715224c845f8b85f
Deepika <deepikagowda.2003@gmail.com>
2026-05-07 11:44:11 +0530

    First commit

---
+++ z.DEMO_JENKINS2/src/test/java/demo/Jenkins.java
@@ -0,0 +1,16 @@
+package demo;
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Jenkins {
+	
+	
+@Test
+	public void test() {
+		Reporter.log(">>>>>>>>>>>>>>>>Hi from Jenkins>>>>>>>>>>>>>>>>>>>>>>",true);
+		
+	
+		
+		
+	}
+}
